aeb2fd882aa13f6455d3591f60c2154d63ebccef
Christo Mabbs <christo.mabbs@gmail.com>
2023-06-30 11:06:09 +0930

    added processing args

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+const { Command } = require("commander");
 const figlet = require("figlet");
 
-console.log(figlet.textSync("Dir Manager"));
+
+const program = new Command();
+
+console.log(figlet.textSync("Bimbus AI"));
+
+program
+  .version("1.0.0")
+  .description("An example CLI for managing a directory")
+  .option("-t [value]", "Open AI Access Token")
+  .option("-i [value]", "Input File")
+  .option("-o [value]", "Output Directory")
+  .parse(process.argv);
+
+const options = program.opts( );
+
