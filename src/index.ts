6439e0fb71301f76b6eda767a5ce0993c12cdf3b
Christo Mabbs <christo.mabbs@gmail.com>
2023-06-30 13:44:07 +0930

    updates on the process

--- src/index.ts
+++ src/index.ts
@@ -28,27 +28,30 @@ program
   .option("-t --token [value]", "Open AI Access Token")
   .option("-i --input [value]", "Input File")
   .option("-o --output [value]", "Output Directory")
-  // Directory Functions
-  .option("-l, --ls  [value]", "List directory contents")
-  .option("-m, --mkdir <value>", "Create a directory")
-  .option("-t, --touch <value>", "Create a file")
+  .option("-v --verbose", "Verbose Output")
   // Final processing
   .parse(process.argv);
 
 // Fetch the options
 const options = program.opts( );
 
-// If not options are specified, show an error and print the help
-if (!process.argv.slice(2).length) {
-  console.error("No options specified!");
-  program.outputHelp();
+// If the -h flag is specified, print the help and exit
+if (options.help) {
+  program.help( );
+  process.exit(0);
+}
+
+// If the token is not specified, show an error and print the help
+if (!options.token) {
+  console.error("Error: Please specify an Open AI token using -t\n");
+  console.error("Run 'bimbus -h' for help\n");
   process.exit(1);
 }
 
 // If the input file is not specified, show an error and print the help
 if (!options.input) {
-  console.error("Please specify an input file");
-  program.outputHelp();
+  console.error("Error: Please specify an input file using -i\n");
+  console.error("Run 'bimbus -h' for help\n");
   process.exit(1);
 }
 
@@ -57,47 +60,29 @@ const openAIToken = options.token;
 const inputFile = path.basename(options.input);
 const outputDir = options.output ? options.output : __dirname;
 
-// Debug
-console.log("Open AI Token: ", openAIToken);
-console.log("Input File: ", inputFile);
-console.log("Output Directory: ", outputDir);
-process.exit(1);
+// Info output
+console.log("Running Bimbus AI...");
+
+// If verbose is specified, print the details
+if (options.verbose) {
+
+  // Print the details
+  console.log("Open AI Token: ", openAIToken);
+  console.log("Input File: ", inputFile);
+  console.log("Output Directory: ", outputDir);
+
+}
+
+// Info output
+console.log("\n");
+
+// Info output
+console.log("Done!");
+
 
 //////////////////////////////////
 // Library of helper functions //
 ////////////////////////////////
 
-// Function to list the contents of a directory
-// const filepath = typeof options.ls === "string" ? options.ls : __dirname;
-// listDirContents(filepath);
-async function listDirContents(filepath: string) {
-  try {
-    const files = await fs.promises.readdir(filepath);
-    const detailedFilesPromises = files.map(async (file: string) => {
-      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
-      const { size, birthtime } = fileDetails;
-      return { filename: file, "size(KB)": size, created_at: birthtime };
-    });
-    const detailedFiles = await Promise.all(detailedFilesPromises);
-    console.table(detailedFiles);
-  } catch (error) {
-    console.error("Error occurred while reading the directory!", error);
-  }
-}
 
-// Function to create a directory
-// createDir(path.resolve(__dirname, options.mkdir));
-function createDir(filepath: string) {
-  if (!fs.existsSync(filepath)) {
-    fs.mkdirSync(filepath);
-    console.log("The directory has been created successfully");
-  }
-}
-
-// Functio to create a file
-createFile(path.resolve(__dirname, options.touch));
-function createFile(filepath: string) {
-  fs.openSync(filepath, "w");
-  console.log("An empty file has been created");
-}
 
